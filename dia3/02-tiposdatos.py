f2d89a2538e7a282bbb6b550aa0cbc899bb7bb01
brenda chombo <bchombourtecho@gmail.com>
2025-10-28 22:21:01 -0500

    dia3

---
+++ dia3/02-tiposdatos.py
@@ -0,0 +1,6 @@
+# programa que suma 2 numeros
+numero1 = int(input("ingrese el primer número entero: "))
+numero2 = int(input("ingrese el segundo numero entero: "))
+suma = numero1 + numero2 
+print(type(suma))
+print(f"La suma de los dos números es: {suma}")
